946271b80a83972a449db20c27329913efe1b59b
WzrdIsTaken <62243693+WzrdIsTaken@users.noreply.github.com>
2021-10-12 21:10:48 +0100

    tired + 2021.1.21f1

---
+++ Techkinesis/Assets/Scripts/Mesh Manipulation/MeshCutout.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+// 
+
+public class MeshCutout : MonoBehaviour
+{
+    // https://www.youtube.com/watch?v=QN39W020LqU&t Follow when not as tired
+}
---
+++ Techkinesis/Assets/Scripts/Mesh Manipulation/MeshCutoutFace.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+// 
+
+public class MeshCutoutFace
+{
+    // https://www.youtube.com/watch?v=QN39W020LqU&t Follow when not as tired
+}
