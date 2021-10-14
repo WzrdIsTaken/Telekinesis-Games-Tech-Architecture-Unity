a1fc67fdf84abb65d6a471e451feea923bd2f599
WzrdIsTaken <62243693+WzrdIsTaken@users.noreply.github.com>
2021-10-14 15:22:34 +0100

    CSG pt1
    problem: mesh too complex

--- Techkinesis/Assets/Scripts/Mesh Manipulation/MeshCutout.cs
+++ Techkinesis/Assets/Scripts/Mesh Manipulation/MeshCutout.cs
@@ -18,7 +18,7 @@ public class MeshCutout : MonoBehaviour
 
     [SerializeField, HideInInspector]
     MeshFilter[] meshFilters;
-    MeshCutoutFace[] terrainFaces;
+    MeshCutoutFace[] meshFaces;
 
     public void GenerateCutout()
     {
@@ -32,6 +32,7 @@ public class MeshCutout : MonoBehaviour
 
         Initialize();
         GenerateMesh();
+        CombineMeshes();
     }
 
     public void OnShapeSettingsUpdated()
@@ -48,7 +49,7 @@ public class MeshCutout : MonoBehaviour
         meshCutoutGenerator = new MeshCutoutGenerator(settings);
 
         if (meshFilters == null || meshFilters.Length == 0) meshFilters = new MeshFilter[6];
-        terrainFaces = new MeshCutoutFace[6];
+        meshFaces = new MeshCutoutFace[6];
 
         Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
 
@@ -64,13 +65,34 @@ public class MeshCutout : MonoBehaviour
                 meshFilters[i].sharedMesh = new Mesh();
             }
 
-            terrainFaces[i] = new MeshCutoutFace(meshCutoutGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            meshFaces[i] = new MeshCutoutFace(meshCutoutGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
         }
     }
 
     void GenerateMesh()
     {
-        foreach (MeshCutoutFace face in terrainFaces) face.ConstructMesh();
+        foreach (MeshCutoutFace face in meshFaces) face.ConstructMesh();
+    }
+
+    void CombineMeshes()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            Destroy(meshFilters[i].gameObject);
+        }
+
+        gameObject.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+        MeshFilter cutoutFilter = gameObject.AddComponent<MeshFilter>();
+
+        cutoutFilter.mesh = new Mesh();
+        cutoutFilter.mesh.CombineMeshes(combine);
+
+        gameObject.AddComponent<MeshCollider>();
     }
 
     IEnumerator DestroyCutout(float lifeTime)
---
+++ Techkinesis/Assets/Scripts/Mesh Manipulation/MeshCutter.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using Parabox.CSG;
+using UnityEngine.InputSystem;
+
+// Performs CSG operations a mesh | Credit: karl- (https://bit.ly/30sqtiL)
+
+public class MeshCutter : MonoBehaviour
+{
+    [SerializeField] GameObject one, two;
+
+    public enum BoolOp
+    {
+        Union,
+        SubtractLR,
+        SubtractRL,
+        Intersect
+    };
+
+    private void Update()
+    {
+        if (Keyboard.current.aKey.wasPressedThisFrame) DoOperation(BoolOp.SubtractLR, one, two);
+    }
+
+    public void DoOperation(BoolOp operation, GameObject left, GameObject right)
+    {
+        Model result;
+
+        switch (operation)
+        {
+            case BoolOp.Union:
+                result = CSG.Union(left, right);
+                break;
+
+            case BoolOp.SubtractLR:
+                result = CSG.Subtract(left, right);
+                break;
+
+            case BoolOp.SubtractRL:
+                result = CSG.Subtract(right, left);
+                break;
+
+            default:
+                result = CSG.Intersect(right, left);
+                break;
+        }
+
+        GameObject composite = new GameObject();
+        composite.AddComponent<MeshFilter>().sharedMesh = result.Mesh;
+        composite.AddComponent<MeshRenderer>().sharedMaterials = result.Materials.ToArray();
+
+        GenerateBarycentric(composite);
+
+        Destroy(left);
+        Destroy(right);
+    }
+
+    void GenerateBarycentric(GameObject go)
+    {
+        Mesh m = go.GetComponent<MeshFilter>().sharedMesh;
+
+        if (m == null) return;
+
+        int[] tris = m.triangles;
+        int triangleCount = tris.Length;
+
+        Vector3[] mesh_vertices = m.vertices;
+        Vector3[] mesh_normals = m.normals;
+        Vector2[] mesh_uv = m.uv;
+
+        Vector3[] vertices = new Vector3[triangleCount];
+        Vector3[] normals = new Vector3[triangleCount];
+        Vector2[] uv = new Vector2[triangleCount];
+        Color[] colors = new Color[triangleCount];
+
+        for (int i = 0; i < triangleCount; i++)
+        {
+            vertices[i] = mesh_vertices[tris[i]];
+            normals[i] = mesh_normals[tris[i]];
+            uv[i] = mesh_uv[tris[i]];
+
+            colors[i] = i % 3 == 0 ? new Color(1, 0, 0, 0) : (i % 3) == 1 ? new Color(0, 1, 0, 0) : new Color(0, 0, 1, 0);
+
+            tris[i] = i;
+        }
+
+        Mesh wireframeMesh = new Mesh();
+
+        wireframeMesh.Clear();
+        wireframeMesh.vertices = vertices;
+        wireframeMesh.triangles = tris;
+        wireframeMesh.normals = normals;
+        wireframeMesh.colors = colors;
+        wireframeMesh.uv = uv;
+
+        go.GetComponent<MeshFilter>().sharedMesh = wireframeMesh;
+    }
+}
