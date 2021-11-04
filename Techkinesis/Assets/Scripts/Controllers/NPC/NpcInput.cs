68a8006cf19bd448e1779400dd1d77bc34ce5c2b
WzrdIsTaken <62243693+WzrdIsTaken@users.noreply.github.com>
2021-11-04 13:18:47 +0000

    finally got round to suppressing that warning..

--- Techkinesis/Assets/Scripts/Controllers/NPC/NpcInput.cs
+++ Techkinesis/Assets/Scripts/Controllers/NPC/NpcInput.cs
@@ -1,16 +1,18 @@
 using System;
 using UnityEngine;
 
-// Manages NPC input
+// Manages NPC input. In this project its just for demonstration 
 
 [CreateAssetMenu(fileName = "NpcInputProvider", menuName = "ScriptableObjects/InputProviders/NPC InputProvider", order = 2)]
 public class NpcInput : ScriptableObject, IInputProvider<InputState>
 {
+#pragma warning disable CS0067  // Disable this warning because I know its not being used - because this sort of logic is out of scope!
     public event Action OnJump;
+#pragma warning restore
 
     public InputState GetState()
     {
-        // TODO Get some cool AI Logic
+        // Some cool AI Logic would go here!
 
         throw new NotImplementedException();
     }
